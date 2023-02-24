dce25cdacd387ce8dc07680685eb71106400efad
Dawid Smolarek <info@butikloox.pl>
2023-02-24 20:38:31 +0100

    error maxium cal stac size exceeded

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
 
       console.log('new Product:', thisProduct);
@@ -111,28 +112,39 @@
       console.log('initOrderForm');
     }
 
+    initAmountWidget() {
+      const thisProduct = this;
+    
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', function() {
+        thisProduct.processOrder();
+        thisProduct.amountWidget.announce(); // dodane wywołanie metody announce
+      });
+    }
+    
+
     processOrder() {
       const thisProduct = this;
-      
+
       // convert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData', formData);
-      
+
       // set price to default price
       let price = thisProduct.data.price;
-      
+
       // for every category (param)...
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         console.log(paramId, param);
-        
+
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
-          
+
           // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default
@@ -147,10 +159,10 @@
               price -= option.price;
             }
           }
-          
+
           // find option image element
           const optionImage = thisProduct.imageWrapper.querySelector(`.${paramId}-${optionId}`);
-          
+
           // check if option image exists
           if (optionImage) {
             if (formData[paramId] && formData[paramId].includes(optionId)) {
@@ -163,12 +175,14 @@
           }
         }
       }
-      
+      // miltiply price by amoount 
+      price *= thisProduct.amountWidget.value;
+
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
-      
+
       console.log("processOrder");
-    }    
+    }
 
     renderInMenu() {
       const thisProduct = this;
@@ -194,6 +208,84 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget)
+    }
+  }
+
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+      
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
+
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+    }
+
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+    setValue(value) {
+      const thisWidget = this;
+    
+      const newValue = parseInt(value);
+
+      /* TODO: Add validation */
+      if(thisWidget.value !== newValue && !isNaN(newValue)) {
+        if(newValue < settings.amountWidget.defaultMin) {
+          thisWidget.value = settings.amountWidget.defaultMin;
+        } else if(newValue > settings.amountWidget.defaultMax) {
+          thisWidget.value = settings.amountWidget.defaultMax;
+        } else {
+          thisWidget.value = newValue;
+        }
+        thisWidget.announce();
+      }
+
+      thisWidget.input.value = thisWidget.value;
+
+    }
+
+    announce() {
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
+    }
+
+    initActions() {
+      let previousValue = this.value;
+      
+      this.input.addEventListener('change', (event) => {
+        this.setValue(event.target.value);
+      });
+    
+      this.input.addEventListener('blur', (event) => {
+        const currentValue = parseInt(this.input.value);
+        if (isNaN(currentValue)) {
+          this.setValue(previousValue);
+        } else {
+          previousValue = currentValue;
+        }
+      });
+    
+      this.linkDecrease.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.setValue(this.value - 1);
+      });
+    
+      this.linkIncrease.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.setValue(this.value + 1);
+      });
     }
   }
 
