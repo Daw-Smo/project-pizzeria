34f603ee6a737e22e87fc053d27408cf06df5d3f
Dawid Smolarek <info@butikloox.pl>
2023-03-01 21:02:00 +0100

    8.4

--- dist/js/script.js
+++ dist/js/script.js
@@ -141,6 +141,7 @@
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
 
       //console.log('initOrderForm');
@@ -214,6 +215,8 @@
       // miltiply price by amoount 
       price *= thisProduct.amountWidget.value;
 
+      thisProduct.priceSingle = price;
+
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
 
@@ -233,6 +236,58 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    addToCart() {
+      const thisProduct = this;
+
+      app.cart.add(thisProduct.prepareCartProduct());
+    }
+
+    prepareCartProduct() {
+      const thisProduct = this;
+
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.data.price,
+        params: thisProduct.prepareCartProductParams()
+      };
+
+      return productSummary;
+    }
+
+    prepareCartProductParams() {
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      const params = {};
+    
+      // for very category (param)
+      for(let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+    
+        // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
+        params[paramId] = {
+          label: param.label,
+          options: {}
+        }
+    
+        // for every option in this category
+        for(let optionId in param.options) {
+          const option = param.options[optionId];
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+    
+          if(optionSelected) {
+            // option is selected!
+            params[paramId].options[optionId] = option.label;
+          }
+        }
+      }
+    
+      return params;
     }
 
     getElements() {
@@ -345,6 +400,7 @@
     
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
     }
     
     initActions() {
@@ -353,6 +409,21 @@
       thisCart.dom.toggleTrigger.addEventListener('click', function() {
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
+    }
+
+    add(menuProduct) {
+      const thisCart = this;
+
+      /* wygeneruj kod HTML na podstawie szablonu */
+      const generatedHTML = templates.cartProduct(menuProduct);
+      /* stwórz element DOM z kodu HTML */
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      /* znajdź kontener dla listy produktów */
+      const productListContainer = thisCart.dom.productList;
+      /* dodaj element do kontenera */
+      productListContainer.appendChild(generatedDOM);
+
+      console.log('adding product', menuProduct);
     }
   }
 
